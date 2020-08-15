2e67fb20089c266791512d07fb1abf77632c15a7
i-am-ren <itsmejeffrey.dev@gmail.com>
2020-08-15 15:01:47 +0900

    Fix Crashed

--- App.js
+++ App.js
@@ -6,6 +6,7 @@
  * @flow strict-local
  */
 
+<<<<<<< HEAD
 import React, {Component, useState, useEffect} from 'react';
 import {
   Appbar,
@@ -14,6 +15,10 @@ import {
   Avatar,
   BottomNavigation,
 } from 'react-native-paper';
+=======
+import React, {Component} from 'react';
+import {Appbar, TextInput, Button, Avatar} from 'react-native-paper';
+>>>>>>> d63b844cf3fd004291035afcc69cece9b1a91f18
 import {
   SafeAreaView,
   StyleSheet,
@@ -35,7 +40,12 @@ import {
 
 import database from '@react-native-firebase/database';
 import firestore from '@react-native-firebase/firestore';
+<<<<<<< HEAD
 import {ListItem, Icon} from 'react-native-elements';
+=======
+import {getUsers} from './oas_api';
+import {ListItem} from 'react-native-elements';
+>>>>>>> d63b844cf3fd004291035afcc69cece9b1a91f18
 
 const reference = database().ref();
 
@@ -77,10 +87,14 @@ class App extends Component {
           <Appbar.Content title={'온라인 알고리즘 스터디'} />
         </Appbar>
         {/* <StatusBar barStyle="dark-content" /> */}
+<<<<<<< HEAD
         <Text
           style={
             styles.title
           }>{`${this.state.userList.length}명이 함께 참여중입니다.`}</Text>
+=======
+    <Text style={styles.title}>{`${this.state.userList.length}명이 함께 참여중입니다.`}</Text>
+>>>>>>> d63b844cf3fd004291035afcc69cece9b1a91f18
         {this.state.isLoaded ? (
           <FlatList
             style={{flex: 1}}
@@ -88,6 +102,7 @@ class App extends Component {
             keyExtractor={(item) => item.id}
             renderItem={({item}) => (
               <ListItem
+<<<<<<< HEAD
                 onPress={() => {
                   console.log(item.name);
                 }}
@@ -99,22 +114,43 @@ class App extends Component {
                 leftAvatar={{
                   placeholderStyle: {
                     backgroundColor: 'grey',
+=======
+              onPress={() => {
+                console.log(item.name);
+              }}
+                title={item.name}
+                subtitle={item.github}
+                leftAvatar={{
+                  placeholderStyle : {
+                    backgroundColor: 'grey'
+>>>>>>> d63b844cf3fd004291035afcc69cece9b1a91f18
                   },
                   rounded: true,
                   title: item.name[0],
                   source: {uri: item.img},
+<<<<<<< HEAD
                   activeOpacity: 0.7,
+=======
+                  activeOpacity :0.7
+>>>>>>> d63b844cf3fd004291035afcc69cece9b1a91f18
                 }}></ListItem>
             )}
           />
         ) : (
+<<<<<<< HEAD
           <View style={[styles.container, styles.horizontal]}>
+=======
+          <View style={[styles.container,  styles.horizontal]}>
+>>>>>>> d63b844cf3fd004291035afcc69cece9b1a91f18
             <ActivityIndicator size="large" />
             <Text>Getting the Users</Text>
             {/* {errors ? <Text>{errors}</Text> : null} */}
           </View>
         )}
+<<<<<<< HEAD
         
+=======
+>>>>>>> d63b844cf3fd004291035afcc69cece9b1a91f18
       </>
     );
   }
@@ -122,6 +158,7 @@ class App extends Component {
 
 const styles = StyleSheet.create({
   horizontal: {
+<<<<<<< HEAD
     flexDirection: 'column',
     justifyContent: 'space-around',
     padding: 10,
--- index.js
+++ index.js
@@ -5,6 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+<<<<<<< HEAD
 import HomePage from './ui/home_page';
+=======
+import Todos from './testApp/Todos';
+>>>>>>> d63b844cf3fd004291035afcc69cece9b1a91f18
 
 AppRegistry.registerComponent(appName,  () => App);
